da837107dc45a8cdd634d3ee5dfe18105c22bcaf
Hlekane <pertuniangobeni2@gmail.com>
2026-04-15 22:34:22 +0200

    Customer Dim

---
+++ Queries/Dim_Tables/1.Dim_Customer_Staging.sql
@@ -0,0 +1,24 @@
+
+-- Drop table if it exist
+
+DROP TABLE [PC_Sales_Stg].[dbo].[Dim_Customer];
+
+-- Create table if it does not exist
+
+CREATE TABLE [dbo].[Dim_Customer](
+	[Customer_ID] INT IDENTITY (1,1) PRIMARY KEY,
+	[Customer_Name] [nvarchar](50) NOT NULL,
+	[Customer_Surname] [nvarchar](50) NOT NULL,
+	[Customer_Contact_Number] [nvarchar](50) NOT NULL,
+	[Customer_Email_Address] [nvarchar](50) NOT NULL
+);
+
+-- Insert data into the customer dimension from the staging dataset, use distinct to remove duplicates
+
+INSERT INTO [PC_Sales_Stg].[dbo].[Dim_Customer] (Customer_Name, Customer_Surname, Customer_Contact_Number, Customer_Email_Address)
+SELECT DISTINCT Customer_Name, Customer_Surname, Customer_Contact_Number, Customer_Email_Address
+FROM [PC_Sales_Stg].[dbo].[PC_sales_dataset_Stg];
+
+-- Check whether the table was succesfully selected
+
+SELECT * FROM [PC_Sales_Stg].[dbo].[Dim_Customer];
